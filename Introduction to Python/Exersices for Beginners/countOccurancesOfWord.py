fb9b738db9ca572729aff06e307dda54774052df
manoldzhermanski <64347529+manoldzhermanski@users.noreply.github.com>
2020-09-16 14:39:37 +0300

    Update countOccurancesOfWord.py

--- Introduction to Python/Exersices for Beginners/countOccurancesOfWord.py
+++ Introduction to Python/Exersices for Beginners/countOccurancesOfWord.py
@@ -3,3 +3,13 @@ def countOccurances(text,word):
     return result
 
 print("Number of occurances:",countOccurances("Emma is good developer. Emma is a writer","Emma"))
+
+def countOccurances2(text,word):
+    print("Given String: ", text)
+    count = 0
+    for i in range(len(text) - 1):
+        count += text[i: i + len(word)] == word
+    return count
+
+count = countOccurances2("Emma is good developer. Emma is a writer","Emma")
+print("Emma appeared ", count, "times")
